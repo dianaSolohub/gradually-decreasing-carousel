3e20ff1d58ae16730cac5809fb0365fe59b62e7e
Diana Solohub <sologubddd111@gmail.com>
2024-02-03 12:39:08 +0200

    change

--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,36 +1,15 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarousel {
-    private final int[] array;
-    private int index;
-    private boolean running;
     public DecrementingCarousel(int capacity) {
-        this.array = new int[capacity];
-        for (int i = 0; i < capacity; i++){
-            array[i] = 0;
-        }
-        this.index = 0;
-        this.running = false;
+
     }
 
     public boolean addElement(int element){
-        if (element <= 0 || index == array.length || running){
-            return false;
-        }else {
-            array[index] = element;
-            index++;
-            return true;
-        }
-    }
-    public CarouselRun run(){
-        if (running){
-            return null;
-        }
-        running = true;
-        return new CarouselRun(array, index, getClassName());
+        throw new UnsupportedOperationException();
     }
 
-    private Object getClassName(){
-        return getClass().getName();
+    public CarouselRun run(){
+       throw new UnsupportedOperationException();
     }
 }
--- src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -4,14 +4,4 @@ public class GraduallyDecreasingCarousel extends DecrementingCarousel{
     public GraduallyDecreasingCarousel(final int capacity) {
         super(capacity);
     }
-
-    @Override
-    public boolean addElement(int element) {
-        return super.addElement(element);
-    }
-
-    @Override
-    public CarouselRun run() {
-        return super.run();
-    }
 }
