cdec196c968517e49ec509767205e01ba4dcb8a8
Diana Solohub <sologubddd111@gmail.com>
2024-02-03 12:36:55 +0200

    change

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -2,12 +2,90 @@ package com.epam.rd.autotasks;
 
 public class CarouselRun {
 
-    public int next() {
-       throw new UnsupportedOperationException();
+    private int currentIndex;
+    private final int countElements;
+    private final int[] array;
+    private final Object className;
+    private int i = 1;
+    public CarouselRun(int[] arr, int count, Object className) {
+        this.currentIndex = 0;
+        this.className = className;
+        if (count != 0){
+            this.array = new int[count];
+            System.arraycopy(arr, 0, array, 0, count);
+        }else {
+            this.array = arr;
+        }
+        this.countElements = count;
     }
 
+    public int next() {
+        if (className.equals("com.epam.rd.autotasks.GraduallyDecreasingCarousel")){
+            return nextG();
+        } else {
+            return nextD();
+        }
+    }
+
+    private int nextG(){
+        if (currentIndex < countElements) {
+            while (currentIndex < countElements && array[currentIndex] <= 0) {
+                currentIndex++;
+            }
+            if (currentIndex < countElements && array[currentIndex] >= 1) {
+                int temp = array[currentIndex];
+                array[currentIndex++] -= i;
+                return temp;
+            }
+        }
+        // Reset index to the beginning if currentIndex reached the end
+        currentIndex = 0;
+        i++;
+
+        // Check if there are non-zero elements from the beginning
+        while (currentIndex < countElements && array[currentIndex] <= 0) {
+            currentIndex++;
+        }
+        if (currentIndex < countElements && array[currentIndex] >= 1) {
+            int temp = array[currentIndex];
+            array[currentIndex++] -= i;
+            return temp;
+        }
+        // No non-zero elements found
+        return -1;
+    }
+
+    private int nextD(){
+        if (currentIndex < countElements) {
+            while (currentIndex < countElements && array[currentIndex] == 0) {
+                currentIndex++;
+            }
+            if (currentIndex < countElements) {
+                return array[currentIndex++]--;
+            }
+        }
+        // Reset index to the beginning if currentIndex reached the end
+        currentIndex = 0;
+
+        // Check if there are non-zero elements from the beginning
+        while (currentIndex < countElements && array[currentIndex] == 0) {
+            currentIndex++;
+        }
+        if (currentIndex < countElements) {
+            return array[currentIndex++]--;
+        }
+        // No non-zero elements found
+        return -1;
+    }
+    private boolean hasNext() {
+        for (int i : array){
+            if (i > 0){
+                return true;
+            }
+        }return false;
+    }
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        return !hasNext();
     }
 
 }
--- src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -4,4 +4,14 @@ public class GraduallyDecreasingCarousel extends DecrementingCarousel{
     public GraduallyDecreasingCarousel(final int capacity) {
         super(capacity);
     }
+
+    @Override
+    public boolean addElement(int element) {
+        return super.addElement(element);
+    }
+
+    @Override
+    public CarouselRun run() {
+        return super.run();
+    }
 }
